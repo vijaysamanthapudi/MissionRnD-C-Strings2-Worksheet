dabf705708b926d27dc82e4557a1ce8a84567d1f
vijaysamanthapudi <vijaysamanthapudi@missionrnd.com>
2016-03-23 09:03:18 +0530

    Submit Commit

--- src/getLastWord.cpp
+++ src/getLastWord.cpp
@@ -9,37 +9,48 @@ Note:Dont modify original string Neglect Spaces at the right end and at left end
 ->Create a new string and return it , Use dynamic memory allocation .
 */
 #include <stdlib.h>
-
+//int move(char str[]);
 char * get_last_word(char * str)
 {
 	if (str == NULL)
 	{
 		return NULL;
 	}
-	int i, j = 0, count = 0, index = 0,len;
-	for (len = 0; str[len]; len++);
-	//int len = strlen(str);
-	for (i = 0; i<len - 1; i++)
-	{
-		if (str[i] == ' '&&str[i + 1] != ' ')
-		{
-			count = 0;
-			index = i + 1;
-		}
-		else if (str[i] != ' ')
-		{
-			count++;
+	int i, last_char = 0, count = 0, index = 0, j = 0;
 
+
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
+		{
+			last_char = i;
+		}
+	}
+	for (i = last_char; i >= 1; i--)
+	{
+		if (str[i] != ' ' && str[i - 1] == ' ')
+		{
+			index = i;
+			break;
+		}
+	}
+	//printf("index is at %c\n last char is at %c", str[index], str[last_char]);
+	char *s = (char *)malloc(sizeof(char)*(last_char - index + 1));
+	for (i = index; i <= last_char; i++)
+	{
+		if (str[i] != ' ')
+		{
+			s[j] = str[i];
+			j++;
+
+			
 		}
 	}
 
-	char *s = (char *)malloc(sizeof(char)*(count + 1));
-	for (i = index; count >= 0; j++, i++, count--)
-	{
-		s[j] = str[i];
-	}
 	s[j] = '\0';
-
 	return s;
-	
 }
+
+
+
+
